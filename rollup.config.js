296a1401eddfc7c702663b92f3d6a1934ee365cd
Alex Bratsos <alexisbratsos@gmail.com>
2020-07-09 10:25:14 +0300

    Update configuration to build once with  multiple outputs

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,20 @@
 import { terser } from "rollup-plugin-terser";
 
-module.exports = [
-  {
-    input: './index.js',
-    plugins: [terser({ecma: 5})],
-    output: {
+module.exports = {
+  input: './index.js',
+  output: [
+    {
       file: 'dist/index.min.js',
       format: 'esm',
       name: "irishPubSub"
-    }
-  },
-  {
-    input: './index.js',
-    plugins: [terser({ecma: 5})],
-    output: {
+    },
+    {
       file: 'dist/index.umd.js',
       format: 'umd',
       name: "irishPubSub"
     }
-  }
-]
+  ],
+  plugins: [
+    terser({ ecma: 5})
+  ]
+}
